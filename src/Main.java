a26d95db4ea9b9dad64ad20f15106646854eb620
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-11-17 05:51:02 +0000

    Add Static File

--- src/Main.java
+++ src/Main.java
@@ -8,37 +8,9 @@ import io.appwrite.Client;
 
 public class Main {
 
-    // This is your Appwrite function
-    // It's executed each time we get a request
     public RuntimeOutput main(RuntimeContext context) throws Exception {
-        // Why not try the Appwrite SDK?
-        //
-        // Client client = new Client();
-        // client
-        //     .setEndpoint("https://cloud.appwrite.io/v1")
-        //     .setProject(System.getenv("APPWRITE_FUNCTION_PROJECT_ID"))
-        //     .setKey(System.getenv("APPWRITE_API_KEY"));
-
-        // You can log messages to the console
-        context.log("Hello, Logs!");
-
-        // If something goes wrong, log an error
-        context.error("Hello, Errors!");
-
-        // The `context.getReq()` object contains the request data
         if (context.getReq().getMethod().equals("GET")) {
-            // Send a response with the res object helpers
-            // `context.getRes().send()` dispatches a string back to the client
-            return context.getRes().send("Hello, World!");
+            return context.getRes().send(Utils.getStaticFile("index.html"));
         }
-
-        Map<String, Object> json = new HashMap<>();
-        json.put("motto", "Build like a team of hundreds_");
-        json.put("learn", "https://appwrite.io/docs");
-        json.put("connect", "https://appwrite.io/discord");
-        json.put("getInspired", "https://builtwith.appwrite.io");
-
-        // `context.getRes().json()` is a handy helper for sending JSON
-        return context.getRes().json(json);
     }
 }
---
+++ src/Utils.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileUtils {
+    /**
+     * Returns the contents of a file in the static folder
+     *
+     * @param fileName The name of the file
+     * @return Contents of static/{fileName}
+     * @throws IOException If an I/O error occurs reading from the file
+     */
+    public static String getStaticFile(String fileName) throws IOException {
+        Path staticFolder = Paths.get("../static/");
+        Path filePath = staticFolder.resolve(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+        return String.join("\n", lines);
+    }
+}
