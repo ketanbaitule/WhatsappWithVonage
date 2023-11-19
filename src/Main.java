c1b7f465c6a7583d6a4d4873333d054487a95a35
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-11-19 16:12:55 +0000

    feat: Add throwIfMissing with environment variables

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ package io.openruntimes.java.src;
 
 import io.openruntimes.java.RuntimeContext;
 import io.openruntimes.java.RuntimeOutput;
-import java.util.HashMap;
-import java.util.Map;
-import io.appwrite.Client;
 
 public class Main {
 
     public RuntimeOutput main(RuntimeContext context) throws Exception {
+        String reqVariables = {
+            "VONAGE_API_KEY",
+            "VONAGE_API_SECRET",
+            "VONAGE_API_SIGNATURE_SECRET",
+            "VONAGE_WHATSAPP_NUMBER"
+        };
+        Utils.throw_if_missing(System.getenv(), reqVariables);
+
         if (context.getReq().getMethod().equals("GET")) {
             return context.getRes().send("Got GET Request");
         }
--- src/Utils.java
+++ src/Utils.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Utils {
     /**
@@ -19,5 +23,24 @@ public class Utils {
         Path filePath = staticFolder.resolve(fileName);
         List<String> lines = Files.readAllLines(filePath);
         return String.join("\n", lines);
+    }
+
+    /**
+     * Throws an error if any of the keys are missing from the map
+     *
+     * @param map  The map to check for missing keys
+     * @param keys The array of keys to check for
+     * @throws Exception If any required fields are missing
+     */
+    public static void throwIfMissing(Map<String, String> map, String[] keys) throws Exception {
+        List<String> missing = new ArrayList<String>();
+        for (String key : keys) {
+            if (!map.containsKey(key) || map.get(key) == null) {
+                missing.add(key);
+            }
+        }
+        if(missing.size() > 0){
+            throw new Exception("Missing required fields: "+String.join(", ", missing));
+        }
     }
 }
