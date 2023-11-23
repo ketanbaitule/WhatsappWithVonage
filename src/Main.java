d4966895f5f83bb2353770c5ca504149a2e13804
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-11-23 16:39:06 +0000

    test: trying to get tokens

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,7 @@ public class Main {
         Object body = context.getReq().getBody();
         Map<String, String> headers = context.getReq().getHeaders();
 
-        context.log(headers.get("authorization"));
+        context.log(headers.get("authorization").split(" "));
 
         // // Verify JWT 
         // try {
