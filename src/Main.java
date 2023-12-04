02b37641ff6e6a159dabbed78d0107622778c9bc
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-12-04 15:24:44 +0000

    feat: verify jwt

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,7 @@ public class Main {
             String token = (headers.get("authorization") != null && !headers.get("authorization").isEmpty()) ? headers.get("authorization").split(" ")[1] : "";
 		context.log("token: "+token+".");
 			SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(System.getenv("VONAGE_API_SIGNATURE_SECRET")));
-			Jwts.parser().json(new GsonDeserializer(gson)).verifyWith(key).build();//Jws<Claims> decoded = Jwts.parser().verifyWith(key).build().parseClaimsJws(token);
+			Jws<Claims> decoded = Jwts.parser().json(new GsonDeserializer(gson)).verifyWith(key).build().parseClaimsJws(token);
 		}catch (JwtException e) {
 			responseMap.put("ok", false);
             responseMap.put("error", "Invalid Token");
