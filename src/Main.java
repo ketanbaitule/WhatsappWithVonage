bf0652897d658c870f843f59912ff48dd67ebd1e
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-12-04 18:31:29 +0530

    fix: correct token

--- src/Main.java
+++ src/Main.java
@@ -64,7 +64,7 @@ public class Main {
 
         // Verify JWT 
         try {
-            String token = (headers.get("authorization") != null && !headers.get("authorization").isEmpty()) ? headers.get("authorization").split(" ")[0] : "";
+            String token = (headers.get("authorization") != null && !headers.get("authorization").isEmpty()) ? headers.get("authorization").split(" ")[1] : "";
 		context.log("token: "+token+".");
 			SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(System.getenv("VONAGE_API_SIGNATURE_SECRET")));
 			Jws<Claims> decoded = Jwts.parser().verifyWith(key).build().parseClaimsJws(token);
